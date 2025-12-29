030e53990d752f656f3a9e75acecce5e0307b545
peraciodias <peraciodias@domain.com>
2025-12-29 18:16:54 -0300

    implementações: CadastroUsuario, Criação de TABELA_USUARIOS. Login, Caixas, criação de TABELA_MOVIMENTACOES_ESTOQUE
    Signed-off-by: peraciodias <peraciodias@domain.com>

---
+++ src/main/java/com/creativex/util/Sessao.java
@@ -0,0 +1,7 @@
+package com.creativex.util;
+
+import com.creativex.model.usuario.Usuario;
+
+public class Sessao {
+    public static Usuario usuarioLogado; // Acessível de qualquer lugar do ERP
+}
