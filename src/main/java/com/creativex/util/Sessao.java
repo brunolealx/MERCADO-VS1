7cd218747b0fa491dacf60068425a6c75fc48115
peraciodias <peraciodias@domain.com>
2026-01-29 00:31:50 -0300

    mudança do Banco de Dados para POSTGRESQL.Alterações diversas pertinentes

--- src/main/java/com/creativex/util/Sessao.java
+++
@@ -1,7 +0,0 @@
-package com.creativex.util;
-
-import com.creativex.model.usuario.Usuario;
-
-public class Sessao {
-    public static Usuario usuarioLogado; // Acessível de qualquer lugar do ERP
-}
